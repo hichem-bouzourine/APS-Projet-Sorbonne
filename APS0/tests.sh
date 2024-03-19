731c1474e8b21e0df20ffa038364a0718435d117
Abdelkader Boumessaoud 21218306 <abdelkader.boumessaoud@etu.sorbonne-universite.fr>
2024-03-20 00:59:41 +0100

    debut de l'eval, a continuer

--- APS0/tests.sh
+++ APS0/tests.sh
@@ -5,6 +5,12 @@ print_color() {
     echo -e "\e[$1m$2\e[0m"
 }
 
+# Nom du fichier de sortie CSV
+output_file="output.csv"
+
+# Effacer le contenu du fichier de sortie s'il existe déjà et écrire l'en-tête CSV
+echo "sample path, correct (yes/no), result" > "$output_file"
+
 # Itérer sur tous les fichiers .aps dans le répertoire Samples et ses sous-répertoires
 find Samples -type f -name '*.aps' | while read -r file; do
     # Exécuter prologTerm pour le fichier en cours
@@ -12,14 +18,12 @@ find Samples -type f -name '*.aps' | while read -r file; do
 
     # Vérifier si le résultat contient le message d'erreur spécifié
     if [[ $result == *"Fatal error: exception Stdlib.Parsing.Parse_error"* ]]; then
-        # Imprimer le nom de fichier en blanc
-        print_color "0;37" "$file"
-        # Imprimer le résultat en rouge
-        print_color "0;31" "$result"
+        # Écrire le résultat en tant que ligne CSV avec "no" pour correct
+        echo "$file, no, \"$result\"" >> "$output_file"
     else
-        # Imprimer le nom de fichier en blanc
-        print_color "0;37" "$file"
-        # Imprimer le résultat en vert
-        print_color "0;32" "$result"
+        # Écrire le résultat en tant que ligne CSV avec "yes" pour correct
+        echo "$file, yes, \"$result\"" >> "$output_file"
     fi
 done
+
+echo "Le résultat a été enregistré dans le fichier $output_file au format CSV."
